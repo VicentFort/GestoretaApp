355f418b3ac7ed768f6f63b66d6e523df44b4e58
vfortro <vfortro@upv.edu.es>
2026-02-12 12:55:14 +0100

    Más documentación de las APIs, añadir tags a los enpdoints.

---
+++ src/main/java/com/vfortro/gestoreta/dto/FoodNeedResultDto.java
@@ -0,0 +1,8 @@
+package com.vfortro.gestoreta.dto;
+
+public class FoodNeedResultDto {
+    private String foodNeedDesc;
+    private String userName;
+    private String userSurname;
+    private String eventName;
+}
