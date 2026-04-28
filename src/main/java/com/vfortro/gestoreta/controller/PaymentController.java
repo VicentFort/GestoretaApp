027e4b1faa6375f2336c3bfbf220490f8d4bc80b
vfortro <vfortro@upv.edu.es>
2026-04-28 12:55:44 +0200

    Añadir endpoints de venta de tickets y creación de tickets al controller

--- src/main/java/com/vfortro/gestoreta/controller/PaymentController.java
+++ src/main/java/com/vfortro/gestoreta/controller/PaymentController.java
@@ -1,4 +1,50 @@
 package com.vfortro.gestoreta.controller;
 
+import com.vfortro.gestoreta.dto.ApiMessageResponse;
+import com.vfortro.gestoreta.dto.payments.PurchaseRequestDto;
+import com.vfortro.gestoreta.dto.payments.coupons.CouponCreateDto;
+import com.vfortro.gestoreta.exceptions.InsufficientStockException;
+import com.vfortro.gestoreta.service.PaymentService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.AccessDeniedException;
+
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/sellCoupons")
+    public ResponseEntity<?> sellCoupon(PurchaseRequestDto request, Authentication auth) {
+        String email = auth.getName();
+        try {
+            paymentService.sellCoupons(request,email);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (AccessDeniedException e) {
+            return new ResponseEntity<>(new ApiMessageResponse(e.getMessage(), false), HttpStatus.FORBIDDEN);
+        } catch (EntityNotFoundException | InsufficientStockException e) {
+            return new ResponseEntity<>(new ApiMessageResponse(e.getMessage(), false), HttpStatus.NOT_FOUND);
+        } catch (IllegalStateException e) {
+            return new ResponseEntity<>(new ApiMessageResponse(e.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/createCoupon")
+    public ResponseEntity<?> createCoupon(CouponCreateDto coupon, Authentication auth) {
+        String email = auth.getName();
+        try {
+            paymentService.createCoupon(coupon, email);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (AccessDeniedException e) {
+            return new ResponseEntity<>(new ApiMessageResponse(e.getMessage(), false),HttpStatus.FORBIDDEN);
+        }
+    }
 }
